ecf427f85fce8e6defed62c3a0296638f178baf6
Canino Developments <ian.canino@gmail.com>
2018-05-17 17:50:07 -0400

    sync 3

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,8 @@
 # This class describes the model class for Genre, with the following relationship:
 # Genres have many songs, and through songs, many artists
+require_relative 'concerns/Slugifiable'
 class Genre < ActiveRecord::Base
+  extend Slugifiable
   has_many :song_genres
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,8 @@
 # This class describes the model class for Song, with the following relationship:
 # Songs belong to one artist and has many genres.
+require_relative 'concerns/Slugifiable'
 class Song < ActiveRecord::Base
-
-  def slug
-    self.name.gsub(" ", "-")
-  end
-
+  extend Slugifiable
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
